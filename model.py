5db5e5c1c7527f755e0c85f844815da21f916ef7
shumi289771 <96087810+shumi289771@users.noreply.github.com>
2024-11-15 09:59:52 +0000

    window ok

--- model.py
+++ model.py
@@ -4,13 +4,26 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 class Model:
-    def __init__(self, ticker_data, scaler = MinMaxScaler(), indicators_to_scale = []):
+    def __init__(self, ticker_data, time_step = 60, scaler = MinMaxScaler(), indicators_to_scale = []):
         self.ticker_data = ticker_data
         self.scaler = scaler 
         self.indicators_to_scale = indicators_to_scale
         self.normalized_data = pd.DataFrame({})  
+        self.time_step = time_step              #taille de la fenetre de calcul des données d'entrainement
+        self.X = []
+        self.y = []
         self.__normalize_data()
+        self.__build_window()
 
+    def __build_window(self):
+        for i in range(len(self.normalized_data) - self.time_step - 1):
+            if self.indicators_to_scale == []:
+                self.X.append(self.normalized_data[i : (i + self.time_step)])
+            else:
+                self.X.append(self.normalized_data[i : (i + self.time_step)][self.indicators_to_scale])
+            self.y.append(self.normalized_data[i + self.time_step : i + self.time_step + 1]['Close'])
+
+        
     def __normalize_data(self):
         if self.indicators_to_scale == []:
             self.normalized_data[self.ticker_data.columns] = self.scaler.fit_transform(self.ticker_data[self.ticker_data.columns])
@@ -30,4 +43,9 @@ class Model:
         print(self.ticker_data.head(5))
         print("normelized data")
         print(self.normalized_data.head(5))
+        print("window (", self.time_step, ") : ")
+        for i in range(len(self.X)):
+            print("Window : ", i)
+            print("X : ", self.X[i]['Close'], " ====> y : ", self.y[i].values[0])
+
            
